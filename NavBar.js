b66523c0a5f59bd039c859b9019847fd1e68f2b7
viniznn777 <viniciuskauan30@hotmail.com>
2022-08-09 19:22:10 -0300

    Portifolio v4

---
+++ NavBar.js
@@ -0,0 +1,32 @@
+let buttonOpen = document.getElementById('OpenMenu');
+let buttonClose = document.getElementById('CloseMenu');
+let menuMobile = document.getElementById('menuMobile');
+let link1 = document.getElementById('link1'), link2 = document.getElementById('link2'),
+link3 = document.getElementById('link3'), link4 = document.getElementById('link4');
+let NavBarOpen = false
+
+window.addEventListener('resize', () => {
+    if (window.innerWidth > 670) {
+        closeMenuMobile();
+    };
+});
+
+function closeMenuMobile() {
+    NavBarOpen = false;
+    menuMobile.style.marginTop = '-500vh';
+    menuMobile.style.animationName = 'CloseMenu';
+};
+
+function openMenuMobile() {
+    NavBarOpen = true;
+    menuMobile.style.marginTop = '0vh';
+    menuMobile.style.animationName = 'OpenMenu';
+};
+
+buttonOpen.addEventListener('click', openMenuMobile);
+buttonClose.addEventListener('click', closeMenuMobile);
+
+link1.addEventListener('click', closeMenuMobile);
+link2.addEventListener('click', closeMenuMobile);
+link3.addEventListener('click', closeMenuMobile);
+link4.addEventListener('click', closeMenuMobile);
---
+++ message.js
@@ -0,0 +1,1 @@
+function Success() {alertify.success('Sent with success!');};
