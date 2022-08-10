e529080b30d709a70e066c91ec4f8ace271a3a60
viniznn777 <viniciuskauan30@hotmail.com>
2022-08-10 11:09:26 -0300

    Portifolio v5

---
+++ scripts/DarkMode.js
@@ -0,0 +1,48 @@
+let button = document.getElementById('DarkMode');
+let icon = document.getElementById('icon');
+
+let particles = document.getElementById('particles-js');
+let main = document.querySelector('main');
+let header = document.querySelector('header');
+let footer = document.querySelector('footer');
+
+
+function LightMode() {
+    if (icon.classList.contains('fa-moon')) {
+        icon.classList.remove('fa-moon');
+        icon.classList.add('fa-sun');
+        particles.classList.add('LightMode');
+        main.classList.add('LightMode');
+        header.classList.add('LightMode');
+        footer.classList.add('LightMode');
+        
+    }
+    else {
+        icon.classList.remove('fa-sun');
+        icon.classList.add('fa-moon');
+        particles.classList.remove('LightMode');
+        main.classList.remove('LightMode');
+        header.classList.remove('LightMode');
+        footer.classList.remove('LightMode');
+    };
+};
+
+function loadTheme() {
+    const darkMode = localStorage.getItem('Light');
+
+    if (darkMode) {
+        LightMode();
+    }
+};
+
+loadTheme();
+
+button.addEventListener('click', function() {
+    LightMode();
+
+    localStorage.removeItem('Light');
+
+    if (icon.classList.contains('fa-sun')) {
+        localStorage.setItem('Light', 1);
+    };
+});
---
+++ scripts/TopButton.js
@@ -0,0 +1,15 @@
+let topButton = document.getElementById('topButton');
+
+document.addEventListener('scroll', () => {
+    if (window.pageYOffset > 900) {
+        topButton.style.opacity = '1';
+    }
+    else {
+        topButton.style.opacity = '0';
+    }
+});
+
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+};
